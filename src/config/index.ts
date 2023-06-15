191c00155916aebe5911a3a9885004a6754ae686
Robert Adams <adamsiii.robert@gmail.com>
2023-06-15 01:03:07 -0700

    rm key

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,6 +7,4 @@ const SSR_ENTRY = isProd ? PROD : DEV;
 
 export const CONFIG = {
   PORT: process.env.PORT || 3001,
-  MAPBOX_PK:
-    "pk.eyJ1IjoiY29tYXRvc2lubyIsImEiOiJjbDFyeW5lNngwYnhwM2NybDhuM2hoOHdtIn0.1lW0Js4LKuMX1SFCzP5J5w",
 };
