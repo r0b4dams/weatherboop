e1bd234590d39c3d04f85349a60bd443c5d07398
Robert Adams <adamsiii.robert@gmail.com>
2023-07-02 21:30:31 -0700

    rename

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import fs from "fs";
 import path from "path";
 import express from "express";
 import compression from "compression";
-// import serveStatic from "serve-static";
 import { createServer } from "vite";
+import { CONFIG } from "./config";
 import { api } from "./api";
 
 // example
 // https://github.com/vitejs/vite-plugin-react/blob/main/playground/ssr-react/server.js
 
-const resolve = (p: string) => path.resolve(__dirname, p);
+// const resolve = (p: string) => path.resolve(__dirname, p);
 
 const port = process.env.PORT || 7456;
 const isProd = process.env.NODE_ENV === "production";
 const DEV = path.join(__dirname, "entry-server.tsx");
 
-const indexHtml = fs.readFileSync(isProd ? "" : "index.html", "utf-8");
+console.log(process.env);
 
 (async () => {
   const app = express();
@@ -37,11 +37,12 @@ const indexHtml = fs.readFileSync(isProd ? "" : "index.html", "utf-8");
       const url = req.originalUrl;
       const ctx = { url: "" };
 
-      const { render } = await vite.ssrLoadModule(DEV);
+      const { render } = await vite.ssrLoadModule(DEV); // entry-server
+      const indexHtml = fs.readFileSync("index.html", "utf-8");
       const template = await vite.transformIndexHtml(url, indexHtml);
       const appHtml = template.replace(`<!--app-->`, await render(url, ctx));
 
-      // <Redirect> rendered
+      // <Redirect /> rendered
       if (ctx.url) {
         res.redirect(301, ctx.url);
         return;
