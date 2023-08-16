6d851663d5646f375f4764d792e2b405cbef5c53
Robert Adams <adamsiii.robert@gmail.com>
2023-08-16 13:49:19 -0700

    rm redux

--- src/client/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import * as rtk from "react-redux";
-import type { RootState, AppDispatch } from ".";
-
-// use these for type intellisense
-export const useAppDispatch: () => AppDispatch = rtk.useDispatch;
-export const useAppSelector: rtk.TypedUseSelectorHook<RootState> = rtk.useSelector;
