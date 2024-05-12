200a256a801f9e800294944f362a829a1e1d7a2d
Robert Adams <r0b4dams@gmail.com>
2024-05-12 11:29:55 -0700

    Swap Latitude and Longitude

--- src/components/DEBUG.tsx
+++ src/components/DEBUG.tsx
@@ -12,10 +12,10 @@ const format = (n: number, digits = 2) => {
 
 export const DEBUG: FC<DEBUG_PROPS> = ({ lng, lat, zoom }) => {
   return (
-    <div className="absolute w-[150px] bg-slate-900 text-slate-100 rounded-br-lg">
+    <div className="absolute w-[150px] text-slate-50 bg-slate-900 rounded-br-lg">
       <p className="my-2">DEBUG</p>
-      <p>Longitude: {format(lng)}</p>
       <p>Latitude: {format(lat)}</p>
+      <p>Longitude: {format(lng)}</p>
       <p>Zoom: {format(zoom)}</p>
     </div>
   );
