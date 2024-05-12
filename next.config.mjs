c8ac4d00da1e8927db0152d6cb0f84d3c73715ac
Robert Adams <r0b4dams@gmail.com>
2024-05-12 11:28:52 -0700

    Add pattern for openweathermap icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
