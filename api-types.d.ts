2ab921bff356677405fbb899c36dd4cf8d44fe61
Robert Adams <r0b4dams@gmail.com>
2024-05-13 15:56:15 -0700

    Add namespace, annotate current response

--- api-types.d.ts
+++ api-types.d.ts
@@ -1,60 +1,95 @@
-interface GeoItem {
-  name: string;
-  state?: string;
-  country: string;
-  local_names: Record<string, string>;
-  lat: number;
-  lon: number;
-}
-
-type GeoResponse = GeoItem[];
-
-interface WeatherItem {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-}
-
-/**
- * https://openweathermap.org/current#fields_json
- */
-interface CurrentWeatherResponse {
-  cod: number;
-  coord: {
-    lon: number;
-    lat: number;
-  };
-  weather: WeatherItem[];
-  main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    humidity: number;
-  };
-  visibility: number;
-  wind: {
-    speed: number;
-    deg: number;
-  };
-  clouds: {
-    all: number;
-  };
-  rain: {
-    "1h": number;
-    "3h": number;
-  };
-  snow: {
-    "1h": number;
-    "3h": number;
-  };
-  dt: number;
-  sys: {
+namespace OWM {
+  interface GeoItem {
+    name: string;
+    state?: string;
     country: string;
-    sunrise: number;
-    sunset: number;
-  };
-  timezone: number;
+    local_names: Record<string, string>;
+    lat: number;
+    lon: number;
+  }
+
+  type GeoResponse = GeoItem[];
+
+  interface WeatherItem {
+    /** Weather condition id */
+    id: number;
+    /** Group of weather parameters (Rain, Snow, Clouds etc.)*/
+    main: string;
+    /** Weather condition within the group.
+     *  https://openweathermap.org/weather-conditions
+     */
+    description: string;
+    /** Weather icon id */
+    icon: string;
+  }
+
+  /**
+   * https://openweathermap.org/current#fields_json
+   */
+  interface CurrentWeatherResponse {
+    /** Response code */
+    cod: number;
+    coord: {
+      /** Longitude of the location */
+      lon: number;
+      /** Latitude of the location */
+      lat: number;
+    };
+    weather: WeatherItem[];
+    main: {
+      /** Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit */
+      temp: number;
+      /**
+       * Temperature. This temperature parameter accounts for the human perception of weather.
+       * Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+       */
+      feels_like: number;
+      // temp_min: number;
+      // temp_max: number;
+
+      /** Atmospheric pressure on the sea level, hPa */
+      pressure: number;
+
+      /** Humidity, % */
+      humidity: number;
+    };
+    /** Visibility, meter. The maximum value of the visibility is 10 km */
+    visibility: number;
+    wind: {
+      /** Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour */
+      speed: number;
+      /** Wind direction, degrees (meteorological) */
+      deg: number;
+      /** Wind gust. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour */
+      gust?: number;
+    };
+    clouds: {
+      /** Cloudiness, % */
+      all: number;
+    };
+    rain?: {
+      /** (where available) Rain volume for the last 1 hour, mm.  */
+      "1h"?: number;
+      /** (where available) Rain volume for the last 3 hours, mm.  */
+      "3h"?: number;
+    };
+    snow?: {
+      /** (where available) Snow volume for the last 1 hour, mm.  */
+      "1h"?: number;
+      /** (where available)Snow volume for the last 3 hours, mm.  */
+      "3h"?: number;
+    };
+    /** Time of data calculation, unix, UTC  */
+    dt: number;
+    sys: {
+      /** Country code (GB, JP etc.) */
+      country: string;
+      /** Sunrise time, unix, UTC */
+      sunrise: number;
+      /** Sunset time, unix, UTC */
+      sunset: number;
+    };
+    /** Shift in seconds from UTC  */
+    timezone: number;
+  }
 }
