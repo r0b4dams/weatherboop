f2fe8c7621b33887e28eae21f1865ba4e692c6f4
Robert Adams <r0b4dams@gmail.com>
2024-05-19 22:10:21 -0700

    Rm SignedOut HOC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
-import { SignInButton, SignUpButton, SignedOut } from "@clerk/nextjs";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="flex flex-col items-center">
         <p>Weatherboop</p>
 
         <div className="flex flex-col">
-          <SignedOut>
-            <SignInButton mode="modal">
-              <button>Sign in with Clerk</button>
-            </SignInButton>
-            <SignUpButton mode="modal">
-              <button>Register with Clerk</button>
-            </SignUpButton>
-          </SignedOut>
+          <SignInButton mode="modal">
+            <button>Sign in with Clerk</button>
+          </SignInButton>
+          <SignUpButton mode="modal">
+            <button>Register with Clerk</button>
+          </SignUpButton>
         </div>
       </div>
     </>
