73ad8bf0105be61f39206a3ab53a14832e7e46a5
Robert Adams <r0b4dams@gmail.com>
2024-05-19 17:02:55 -0700

    Abbreviate request var

--- src/app/api/boop/route.ts
+++ src/app/api/boop/route.ts
@@ -3,8 +3,8 @@ import { type NextRequest } from "next/server";
 import { OPENWEATHERMAP_APPID } from "~/config";
 import { getWeather } from "./helpers";
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
   searchParams.append("appid", OPENWEATHERMAP_APPID);
   const weather = await getWeather(searchParams.toString());
   return Response.json(weather);
