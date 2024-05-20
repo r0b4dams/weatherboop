e1f8f5f3a2609d1bdeec260be0fb5037b8c658c1
Robert Adams <r0b4dams@gmail.com>
2024-05-19 17:01:55 -0700

    Update middleware to protect routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,25 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 export const config = {
-  matcher: ["/((?!.*..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/(api|trpc)(.*)", "/assets(.*)"],
 };
+
+const isPublic = createRouteMatcher("/");
+
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  const pathname = new URL(req.url).pathname;
+
+  if (userId) {
+    if (pathname === "/") {
+      return NextResponse.redirect(new URL("/map", req.url));
+    }
+  } else if (!isPublic(req)) {
+    if (pathname.startsWith("/api") || pathname.startsWith("/assets")) {
+      return Response.json({ message: "unauthorized" }, { status: 401 });
+    }
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  return NextResponse.next();
+});
