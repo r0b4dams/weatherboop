f54d297917192a004d7dc8ccd177e8a9becdebe7
Robert Adams <r0b4dams@gmail.com>
2024-05-22 16:59:29 -0700

    Update imports

--- src/components/UserInterface.tsx
+++ src/components/UserInterface.tsx
@@ -1,6 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 
-export function UI() {
+export function UserInterface() {
   return (
     <div className="absolute z-10 right-5 top-5">
       <UserButton userProfileMode="navigation" userProfileUrl="/profile" />
--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { Mapbox } from "./mapbox";
-export { UI } from "./ui";
