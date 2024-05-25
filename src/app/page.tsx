efcfb40712ba5a8408aebc415bd4e9fc4561bf69
Robert Adams <r0b4dams@gmail.com>
2024-05-24 21:03:17 -0700

    Render auth buttons in centered card, add skeleton loaders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,42 @@
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton } from "@clerk/nextjs";
+
+import { Button } from "~/components/ui/button";
+import { Skeleton } from "~/components/ui/skeleton";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { Divider } from "~/components/Divider";
+import { Footer } from "~/components/Footer";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex flex-col items-center">
-        <p>Weatherboop</p>
+    <div className="flex flex-col flex-grow">
+      <Card className="m-auto w-80 shadow-xl">
+        <CardHeader>
+          <CardTitle className="text-center">🌤️ Weatherboop ⛈️</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ClerkLoading>
+            <div className="flex flex-col justify-between min-h-44">
+              <Skeleton className="w-full h-10" />
+              <Divider />
+              <Skeleton className="w-full h-10" />
+            </div>
+          </ClerkLoading>
 
-        <div className="flex flex-col">
-          <SignInButton mode="modal">
-            <button>Sign in with Clerk</button>
-          </SignInButton>
-          <SignUpButton mode="modal">
-            <button>Register with Clerk</button>
-          </SignUpButton>
-        </div>
-      </div>
-    </>
+          <ClerkLoaded>
+            <div className="flex flex-col justify-between min-h-44">
+              <SignInButton mode="modal">
+                <Button>Login</Button>
+              </SignInButton>
+              <Divider text="or" />
+              <SignUpButton mode="modal">
+                <Button>Create new account</Button>
+              </SignUpButton>
+            </div>
+          </ClerkLoaded>
+        </CardContent>
+      </Card>
+
+      <Footer />
+    </div>
   );
 }
