021dabb74050ca572e6171ebb7b991a59545d88c
Robert Adams <r0b4dams@gmail.com>
2024-05-25 16:35:39 -0700

    Add getCurrentWeather action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,11 +3,33 @@
 import { cookies } from "next/headers";
 import { clerkClient } from "@clerk/nextjs/server";
 
-/**
- * Server action to delete a user.
- * @param userId The id of the user to delete
- * @param sessionId The id of the session to revoke
- */
+import { searchSchema, type WeatherSearch } from "./schema";
+import { OPENWEATHERMAP_APPID, OPENWEATHERMAP_CURRENT_URL } from "~/config";
+import { formatCurrentWeather } from "./utils";
+
+export async function getCurrentWeather(search: WeatherSearch) {
+  try {
+    searchSchema.parse(search);
+
+    const params = new URLSearchParams({
+      lon: search.coordinates[0].toString(),
+      lat: search.coordinates[1].toString(),
+      units: search.units || "standard",
+    });
+    params.append("appid", OPENWEATHERMAP_APPID);
+
+    const url = `${OPENWEATHERMAP_CURRENT_URL}?${params.toString()}`;
+    const data: OWM.WeatherResponseData = await fetch(url).then((res) => res.json());
+
+    return formatCurrentWeather(data);
+  } catch (error) {
+    if (error instanceof Error) {
+      return { status: 400, msg: error.message };
+    }
+    return { status: 500, msg: "something went wrong..." };
+  }
+}
+
 export async function deleteUser(userId: string, sessionId: string) {
   const store = cookies();
   store.getAll().forEach((cookie) => {
