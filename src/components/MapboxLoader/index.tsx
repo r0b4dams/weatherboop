787c2c1ccf2ad20b312be37f8451de885e37d03a
Robert Adams <r0b4dams@gmail.com>
2024-05-25 16:40:56 -0700

    Update loader animation

--- src/components/MapboxLoader/index.tsx
+++ src/components/MapboxLoader/index.tsx
@@ -1,5 +1,9 @@
 import "./map-loader-styles.css";
 
 export function MapboxLoader() {
-  return <div className="mapbox-loader"></div>;
+  return (
+    <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center">
+      <div className="map-loader"></div>
+    </div>
+  );
 }
