b9a58be649a6ddcfc768e758b6a13831ee8e7760
Robert Adams <r0b4dams@gmail.com>
2024-05-25 16:35:53 -0700

    Move formatCurrentWeather to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCurrentWeather(current: OWM.WeatherResponseData) {
+  return {
+    temp: current.main.temp,
+    humidity: current.main.humidity,
+    pressure: current.main.pressure,
+    weather: current.weather,
+  };
+}
