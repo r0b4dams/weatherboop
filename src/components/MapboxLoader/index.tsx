5394487bfd005ddaf2648166864a523fc5d97785
Robert Adams <r0b4dams@gmail.com>
2024-05-26 20:58:49 -0700

    Update map loader, add refresh button

--- src/components/MapboxLoader/index.tsx
+++ src/components/MapboxLoader/index.tsx
@@ -1,9 +1,28 @@
 import "./map-loader-styles.css";
 
+import { useEffect, useState } from "react";
+
+import { Button } from "../ui/button";
+
 export function MapboxLoader() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 5000);
+  }, []);
+
   return (
-    <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center">
-      <div className="map-loader"></div>
+    <div className="flex flex-col z-[999] absolute top-0 right-0 bottom-0 left-0 justify-center items-center bg-white">
+      <div className="loader"></div>
+      {show && (
+        <div>
+          <Button variant="outline" onClick={() => window.location.reload()}>
+            Reload
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
