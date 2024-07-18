fbbd9ea5bac2eb6ac4e79c94abce1aa83e6dec6c
Robert Adams <r0b4dams@gmail.com>
2024-07-18 15:48:28 -0700

    prettier

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const MAPBOX_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAPBOX_KEY || ""
+export const MAPBOX_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAPBOX_KEY || "";
 export const OPENWEATHERMAP_APPID = process.env.OPENWEATHERMAP_APPID || "";
 export const OPENWEATHERMAP_BASE_URL = "https://api.openweathermap.org";
 export const OPENWEATHERMAP_CURRENT_URL = `${OPENWEATHERMAP_BASE_URL}/data/2.5/weather`;
