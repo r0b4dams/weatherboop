049ec900d537a9bc2442dbeab27276b1b0de7d8c
Robert Adams <r0b4dams@gmail.com>
2024-07-30 17:05:22 -0700

    Add logic to get local time from OWM request

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,9 +19,45 @@ export function cn(...inputs: ClassValue[]) {
 
 export function formatCurrentWeather(current: OWM.WeatherResponseData) {
   return {
+    dt: getDateTime(current.dt, current.timezone),
     temp: current.main.temp,
     humidity: current.main.humidity,
     pressure: current.main.pressure,
     weather: current.weather,
   };
+}
+
+interface IDate {
+  weekday: string
+  day: string;
+  month: string;
+  year: string;
+}
+interface ITime {
+  hour: string;
+  minute: string;
+  second: string;
+}
+
+export interface IDateTime {
+  date: IDate,
+  time: ITime
+}
+
+function getDateTime(dt: number, offset: number): IDateTime {
+  const [weekday, day, month, year, time] = new Date((dt + offset) * 1000).toUTCString().split(" ")
+  const [hour, minute, second] = time.split(":")
+  return {
+    date: {
+      weekday: weekday.substring(0, weekday.length - 1),
+      day,
+      month,
+      year
+    },
+    time: {
+      hour,
+      minute,
+      second
+    }
+  }
 }
