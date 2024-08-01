4fae58fd6f66e584a86cd0b37eed1dd67fa77513
Robert Adams <r0b4dams@gmail.com>
2024-07-31 18:05:32 -0700

    Add time format button

--- src/components/UserInterface.tsx
+++ src/components/UserInterface.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 export function UserInterface() {
   return (
-    <div className="absolute z-10 right-5 top-5">
+    <div className="absolute z-10 right-5 top-5 flex flex-col items-center">
       <UserButton userProfileMode="navigation" userProfileUrl="/profile" />
+      <Button>Time</Button>
     </div>
   );
 }
