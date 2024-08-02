bcfed62b9ab0b231659eca6e064144b70013373e
Robert Adams <r0b4dams@gmail.com>
2024-08-01 19:26:27 -0700

    Add TimeFormat type, update comments

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,13 +1,17 @@
 import { z } from "zod";
 
 /**
- * [longitude, latitude]
+ * Follows Mapbox's convention: [ longitude, latitude ]
  */
 export type Coordinates = z.infer<typeof coordSchema>;
 export const coordSchema = z.tuple([
+  // The LngLat class on a Mapbox event includes a wrap method to clamp lng bounds to [-180, 180]
   z.number().min(-180).max(180),
   z.number().min(-90).max(90),
 ]);
+
+export type TimeFormat = z.infer<typeof timeSchema>
+export const timeSchema = z.enum(["12", "24"])
 
 export type Units = z.infer<typeof unitSchema>;
 export const unitSchema = z.enum(["standard", "metric", "imperial"]);
