23df576f995bfbd3118a4f31034bfb0eb4504491
Robert Adams <r0b4dams@gmail.com>
2024-08-03 14:30:14 -0700

    16 - Toggle unit type (#38)

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,14 +1,16 @@
 import { create } from "zustand";
 import { z } from "zod";
 
-import { timeSchema } from "./schema";
+import { timeSchema, unitSchema } from "./schema";
 
 const stateSchema = z.object({
   timeFormat: timeSchema,
+  units: unitSchema,
 });
 
 const actionSchema = z.object({
   setTimeFormat: z.function().args(timeSchema).returns(z.void()),
+  setUnits: z.function().args(unitSchema).returns(z.void()),
 });
 
 const storeSchema = z.intersection(stateSchema, actionSchema);
@@ -17,4 +19,6 @@ type Store = z.infer<typeof storeSchema>;
 export const useStore = create<Store>((set) => ({
   timeFormat: "24",
   setTimeFormat: (timeFormat) => set(() => ({ timeFormat })),
+  units: "imperial",
+  setUnits: (units) => set(() => ({ units })),
 }));
