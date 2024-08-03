ae5b3fb165efbfb8702050403cc827bdc1ed81d9
Robert Adams <r0b4dams@gmail.com>
2024-08-03 14:06:45 -0700

    Add button and handler to toggle unit types

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -4,19 +4,26 @@ import { Button } from "./ui/button";
 import { useStore } from "~/lib/store";
 
 export function UI() {
-  const [timeFormat, setTimeFormat] = useStore((state) => [
+  const [timeFormat, setTimeFormat, units, setUnits] = useStore((state) => [
     state.timeFormat,
     state.setTimeFormat,
+    state.units,
+    state.setUnits,
   ]);
 
-  const handleUpdateTimeFormat = () => {
+  const handleSetTimeFormat = () => {
     timeFormat === "24" ? setTimeFormat("12") : setTimeFormat("24");
+  };
+
+  const handleSetUnits = () => {
+    units === "imperial" ? setUnits("metric") : setUnits("imperial");
   };
 
   return (
     <div className="absolute z-10 right-5 top-5 flex flex-col items-center">
       <UserButton userProfileMode="navigation" userProfileUrl="/profile" />
-      <Button onClick={handleUpdateTimeFormat}>Time format: {timeFormat}</Button>
+      <Button onClick={handleSetTimeFormat}>Time format: {timeFormat}</Button>
+      <Button onClick={handleSetUnits}>Units: {units}</Button>
     </div>
   );
 }
